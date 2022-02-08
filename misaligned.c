e0bc8c3de074e3cf6bf8b57bcd9757eabfa6a5c6
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-08 11:51:30 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -85,13 +85,13 @@ int main() {
     GetColorPairNum_FunctionPtr = GetColorFromPairNumber;
     CopyColorPairs_FunctionPtr = CopyColorPairs;
     PrintColorMapFuncPtr = printColorMap;
-    result = PrintColorMapFuncPtr(CopyColorPairs_FunctionPtr);
+    result = PrintColorMapFuncPtr(CopyColorPairs);
     assert(result == 25);
     
     GetColorPairNum_FunctionPtr = GetColorFromPairNumber_Stub;
     CopyColorPairs_FunctionPtr = CopyColorPairs_Stub;
     PrintColorMapFuncPtr = printColorMap_Stub;
-    result = PrintColorMapFuncPtr(CopyColorPairs_FunctionPtr);
+    result = PrintColorMapFuncPtr(CopyColorPairs_Stub);
     assert(result == 25);
     printf("All is well (maybe!)\n");
     return 0;
